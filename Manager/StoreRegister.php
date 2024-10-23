a6a9dd6e38356e31ee88420d48ee77bcad6b6412
NGUYENTHISUONG <suongnguyen182001@gmail.com>
2024-10-23 14:42:08 +0900

    a

--- Manager/StoreRegister.php
+++ Manager/StoreRegister.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="./styles/StoreRegister.css">
     <title>登録</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.5.7/lottie.min.js"></script>
