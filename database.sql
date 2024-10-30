c5c52105dc63a3f81ecf38dd3cf89b028dbe2772
NGOCHAI <phamngochai0209@gmail.com>
2024-10-30 10:58:27 +0900

    s

--- database.sql
+++ database.sql
@@ -45,6 +45,14 @@ CREATE TABLE category (
     cname VARCHAR(100) NOT NULL COLLATE utf8mb4_0900_ai_ci
 );
 
+CREATE TABLE pending_users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    token VARCHAR(64) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 
 
 
